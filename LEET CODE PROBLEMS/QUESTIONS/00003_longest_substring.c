3d6236df3cb855489536bf8c9966d8e763992d93
Deep Mandal <deepmandal9091@gmail.com>
2025-01-19 13:55:12 +0530

    Update and rename q010_longest_substring.c to 00003_longest_substring.c

--- LEET CODE PROBLEMS/QUESTIONS/00003_longest_substring.c
+++ LEET CODE PROBLEMS/QUESTIONS/00003_longest_substring.c
@@ -1,28 +1,23 @@
-/* LONGEST SUBSTRING WITHOUT REPEATING CHARACTERS Given a string s, find the length of 
-the longest substring without repeating characters.
-Example 1:Input: s = "abcabcbb",Output: 3
-Explanation: The answer is "abc", with the length of 3.
-Example 2: Input: s = "bbbbb",Output: 1
-Explanation: The answer is "b", with the length of 1.*/
+//LEET CODE QUESTION 3
 #include<stdio.h>
 #include<string.h>
 int main(){
-    char str[20];
+    char str[20]="";
     int i,j,len,count=1,maxCount=1,x=0;
     printf("Enter string : ");
-    scanf("%s",str);
+    //scanf("%s",str);
     len = strlen(str);
-    if(len==0){ // for str don't have any character
+    if(len==0){
         printf("The length of the longest substring without repeating characters 0");
         return 0;
     }
     for(i=1;i<len;i++){
         count=1;
-        for(j=x;j<i;j++){   // check for 0 ,1 ,.....(index)
-            if(str[j]!=str[i])  
+        for(j=x;j<i;j++){
+            if(str[j]!=str[i])
                 count++;
             else{
-                x=j+1;  // do not check at first when repeated come ,check at last repeated number  
+                x=j+1;
                 break;
             }
                 
