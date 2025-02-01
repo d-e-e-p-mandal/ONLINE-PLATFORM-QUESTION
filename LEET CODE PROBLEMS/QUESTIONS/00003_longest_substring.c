a2c7b2b84f8dd357055bb631b0250df6505a6388
Deep Mandal <deepmandal9091@gmail.com>
2025-02-01 21:52:58 +0530

    Update 00003_longest_substring.c

--- LEET CODE PROBLEMS/QUESTIONS/00003_longest_substring.c
+++ LEET CODE PROBLEMS/QUESTIONS/00003_longest_substring.c
@@ -1,16 +1,11 @@
-//LEET CODE QUESTION 3
+//LEETCODE QUESTION 3
 #include<stdio.h>
 #include<string.h>
-int main(){
-    char str[20]="";
+int lengthOfLongestSubstring(char* str) {
     int i,j,len,count=1,maxCount=1,x=0;
-    printf("Enter string : ");
-    //scanf("%s",str);
     len = strlen(str);
-    if(len==0){
-        printf("The length of the longest substring without repeating characters 0");
+    if(len==0)
         return 0;
-    }
     for(i=1;i<len;i++){
         count=1;
         for(j=x;j<i;j++){
@@ -24,11 +19,16 @@ int main(){
         }
         maxCount=(maxCount<count)?count:maxCount;
     }
-    printf("The length of the longest substring without repeating characters %d",maxCount);
+    return maxCount;
+}
+
+int main(){
+    char s[] = "pwwkew";
+    
+    printf("The length of the longest substring without repeating characters %d",lengthOfLongestSubstring(s));
     return 0;
 }
 /*
 OUTPUT :
-Enter string : pwwkew
 The length of the longest substring without repeating characters 3
 */
