b53457f90361dc3b6958b06e6412ed65202ea70f
“Deep <deepmandal9091@gmail.com>
2025-03-12 00:47:59 +0530

    add

--- LEET CODE PROBLEMS/QUESTIONS/00680_valid_palindrome_2.c
+++ LEET CODE PROBLEMS/QUESTIONS/00680_valid_palindrome_2.c
@@ -1,1 +1,36 @@
 // LEETCODE - 680 : Valid Palindrome 2 
+#include <stdio.h>
+#include <stdbool.h>
+#include <string.h>
+
+bool isPalindrome(char *s, int i, int j) {
+    while ( i < j) {
+        if (s[i] != s[j]) {
+            return false;
+        }
+        i++;j--;
+    }
+    return true;
+}
+
+bool validPalindrome(char *s) {
+    int i = 0, j = strlen(s) - 1;
+
+    while (i < j) {
+        if (s[i] != s[j]) {
+            // Try skipping either left or right character
+            return isPalindrome(s, i + 1, j) || isPalindrome(s, i, j - 1);
+        }
+        i++;j--;
+    }
+    return true; // It's already a palindrome
+}
+
+// Driver code for testing
+int main() {
+    char s[] = "abca";
+
+    printf("%s -> %s\n", s, validPalindrome(s) ? "true" : "false");
+
+    return 0;
+}
