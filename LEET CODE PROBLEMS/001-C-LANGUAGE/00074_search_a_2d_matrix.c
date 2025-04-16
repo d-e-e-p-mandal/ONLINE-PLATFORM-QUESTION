46a309df2f08254667c235ffda950ce9c7a4c466
“Deep <deepmandal9091@gmail.com>
2025-04-17 01:35:45 +0530

    sdd

--- LEET CODE PROBLEMS/001-C-LANGUAGE/00074_search_a_2d_matrix.c
+++ LEET CODE PROBLEMS/001-C-LANGUAGE/00074_search_a_2d_matrix.c
@@ -59,6 +59,25 @@ int main() {
 
 /*Time Complexity : O(log(m) + log(n)) = O(log(mn))
 Space Complexity : O(1)*/
+/* Method : (c)
+bool searchMatrix(int** matrix, int matrixSize, int* matrixColSize, int target) {
+    int m = matrixSize;
+    int n = *matrixColSize;
+    int left = 0, right = m * n - 1;
+
+    while (left <= right) {
+        int mid = left + (right - left) / 2;
+        int mid_value = matrix[mid / n][mid % n];
+
+        if (mid_value == target)
+            return true;
+        else if (mid_value < target)
+            left = mid + 1;
+        else
+            right = mid - 1;
+    }
+    return false;
+}*/
 
 /*(extra : not recomended)
 bool searchMatrix(int** matrix, int matrixSize, int* matrixColSize, int target) {
