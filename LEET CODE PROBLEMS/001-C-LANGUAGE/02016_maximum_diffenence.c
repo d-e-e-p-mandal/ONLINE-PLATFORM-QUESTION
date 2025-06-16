0b933c519a3e3e2645ab0a468f3aa5f50748f7e8
“Deep <deepmandal9091@gmail.com>
2025-06-16 23:26:25 +0530

    add

---
+++ LEET CODE PROBLEMS/001-C-LANGUAGE/02016_maximum_diffenence.c
@@ -0,0 +1,26 @@
+//LeetCode : Maximum Difference Between Increasing Elements
+
+#include <stdio.h>
+
+int maximumDifference(int* nums, int numsSize) {
+    int i,diff = -1,max=-1,minIndex = 0;
+    for(i=1; i<numsSize; i++){
+        if(nums[minIndex] >= nums[i]) //if current element is less than previous element
+            minIndex = i;
+        else
+            diff = nums[i]-nums[minIndex];//calulate difference
+
+        if(max < diff) max = diff;//store max diff
+    }   
+    return max;   
+}
+
+int main() {
+    int nums[] = {7, 1, 5, 4};
+    int size = sizeof(nums) / sizeof(nums[0]);
+
+    int result = maximumDifference(nums, size);
+    printf("Maximum Difference: %d\n", result);
+
+    return 0;
+}
