882aae4540ca8194e456028d8671a425c3022493
“Deep <deepmandal9091@gmail.com>
2025-06-29 11:41:56 +0530

    add

--- LEET CODE PROBLEMS/002 C++ LANGUAGE/00062_unique_paths_004.cpp
+++ LEET CODE PROBLEMS/002 C++ LANGUAGE/00062_unique_paths_004.cpp
@@ -8,8 +8,10 @@ public:
     // from (0,0) to (m,n) => (m+n)!/m!*n! (maths : P&C)
     int uniquePaths(int m, int n) { // here (0,0) to (m-1,n-1)
         long long res = 1;
-        for(int i = 1; i <= m - 1; i++) {
-            res = res * (n - 1 + i) / i;
+        int total = m+n-2;
+        int minimum = min(m,n);
+        for(int i = 1; i < minimum; i++) {
+            res = res * (total - i + 1) / i;
         }
         return (int)res;
     }
@@ -28,3 +30,18 @@ int main() {
 
     return 0;
 }
+
+/* Time Complexity : O(min(M,N))
+Space Complexity : O(1)*/
+
+/*Another Math approach:
+public:
+    // from (0,0) to (m,n) => (m+n)!/m!*n! (maths : P&C)
+    int uniquePaths(int m, int n) { // here (0,0) to (m-1,n-1)
+        long long res = 1;
+        for(int i = 1; i <= m - 1; i++) {
+            res = res * (n - 1 + i) / i;
+        }
+        return (int)res;
+    }
+}; */
